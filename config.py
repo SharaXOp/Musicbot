a7ed251342b7aa32e0b3cef54ba88e7f3882cf4a
Shara X Op <160161353+SharaXOp@users.noreply.github.com>
2024-02-16 13:05:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/devilxdz/sharaXmuisc",
+    "https://github.com/SharaXOp/Muiscbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
